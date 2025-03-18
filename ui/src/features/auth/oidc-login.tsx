fd31822d352dff9ada0e1795078873208bdf5b02
Stefan Richter <stefan@02strich.de>
2025-03-17 22:01:00 -0700

    feat: Add OIDC State
    When using PKCE it is recommended to send and validate a state variable to make sure the response fits. While this is not required by the specification, it is recommended, and turns out to be required by some implementations so doing it.

--- ui/src/features/auth/oidc-login.tsx
+++ ui/src/features/auth/oidc-login.tsx
@@ -3,6 +3,7 @@ import { Button, notification } from 'antd';
 import {
   discoveryRequest,
   processDiscoveryResponse,
+  generateRandomState,
   generateRandomCodeVerifier,
   calculatePKCECodeChallenge,
   validateAuthResponse,
@@ -25,6 +26,7 @@ import {
 } from './oidc-utils';
 
 const codeVerifierKey = 'PKCE_code_verifier';
+const stateKey = 'PKCE_state';
 
 type Props = {
   oidcConfig: OIDCConfig;
@@ -91,6 +93,8 @@ export const OIDCLogin = ({ oidcConfig }: Props) => {
 
     const code_verifier = generateRandomCodeVerifier();
     sessionStorage.setItem(codeVerifierKey, code_verifier);
+    const state = generateRandomState();
+    sessionStorage.setItem(stateKey, state);
 
     const code_challenge = await calculatePKCECodeChallenge(code_verifier);
     const url = new URL(as.authorization_endpoint);
@@ -99,6 +103,7 @@ export const OIDCLogin = ({ oidcConfig }: Props) => {
     url.searchParams.set('code_challenge_method', 'S256');
     url.searchParams.set('redirect_uri', redirectURI);
     url.searchParams.set('response_type', 'code');
+    url.searchParams.set('state', state);
     url.searchParams.set('scope', getOIDCScopes(oidcConfig, as).join(' '));
 
     window.location.replace(url.toString());
@@ -108,19 +113,22 @@ export const OIDCLogin = ({ oidcConfig }: Props) => {
   React.useEffect(() => {
     (async () => {
       const code_verifier = sessionStorage.getItem(codeVerifierKey);
+      const state = sessionStorage.getItem(stateKey);
       const searchParams = new URLSearchParams(location.search);
 
-      if (!as || !code_verifier || !searchParams.get('code') || !searchParams.get('code')) {
+      if (
+        !as ||
+        !code_verifier ||
+        !state ||
+        !searchParams.get('code') ||
+        !searchParams.get('code') ||
+        !searchParams.get('state')
+      ) {
         return;
       }
 
-      // Delete empty state
-      if (searchParams.get('state') === '') {
-        searchParams.delete('state');
-      }
-
       try {
-        const params = validateAuthResponse(as, client, searchParams);
+        const params = validateAuthResponse(as, client, searchParams, state);
 
         const response = await authorizationCodeGrantRequest(
           as,
